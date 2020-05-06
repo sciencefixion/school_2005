3fc559f6c8458b054b1f87231762839bec16f187
Rion Hutsell <st.gish@gmail.com>
2020-05-06 13:29:20 -0600

    Add school.rb and school_test.rb with Iterations 1-2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
+class School
+  attr_reader :start_time, :hours_in_school_day, :student_names
 
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+
+  end
+
+  def add_student_name(student_name)
+    @student_names << student_name
+  end
+
+  def end_time
+  end_hour = @hours_in_school_day + @start_time.to_i
+  end_hour.to_s + ":00"
+  end
+
+end
